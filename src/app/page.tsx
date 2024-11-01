2c34b3ae4278b5ae5175977c7572ddbce7884403
Josh Shavers <shaversj@gmail.com>
2024-11-01 18:05:21 -0500

    Add transforms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,50 @@ import ArrowDownIcon from "@/components/icons/ArrowDownIcon";
 import { useState } from "react";
 import ArrowUpIcon from "@/components/icons/ArrowUpIcon";
 
+type MenuState = "initial" | "menuOpen" | "menuClosed";
+
 export default function Home() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [menuState, setMenuState] = useState<MenuState>("initial");
+
+  const toggleMenu = () => {
+    setMenuState((prevState: MenuState) => {
+      if (prevState === "initial" || prevState === "menuClosed") {
+        return "menuOpen";
+      } else {
+        return "menuClosed";
+      }
+    });
+  };
+
+  const quoteVarients = {
+    base: "w-[540px] text-base font-normal text-white",
+    initial: "w-[540px] text-base font-normal text-white",
+    menuOpen: "w-[540px] opacity-0 transition-all duration-[1500ms]",
+    menuClosed: "w-[540px] opacity-100 transition-all duration-[1000ms] ease-in",
+  };
+
+  const timeVarients = {
+    base: "flex items-end",
+    initial: "pt-[233px]",
+    menuOpen: "-translate-y-[120px] transition-all duration-1000",
+    menuClosed: "pt-[20px] translate-y-[233px] pb-[98px] transition-all duration-1000",
+  };
+
+  const subMenuVarients = {
+    base: "bg-white/50 backdrop-blur-xl",
+    initial: "mt-[233px] translate-y-[70px]",
+    menuOpen: "-translate-y-[70px] transition-all duration-1000",
+    menuClosed: "mt-[233px] translate-y-[70px] transition-all duration-1000",
+  };
 
   return (
-    <div className={"h-[800px] w-[1440px] bg-black/40 bg-[url('/images/desktop/bg-image-daytime.jpg')] bg-cover bg-blend-overlay"}>
+    <div
+      className={
+        "h-[800px] w-[1440px] overflow-hidden bg-black/40 bg-[url('/images/desktop/bg-image-daytime.jpg')] bg-auto pt-[56px] bg-blend-overlay"
+      }
+    >
       <div className="px-[165px]">
-        <div className={`font-inter w-[540px] text-base font-normal text-white ${isMenuOpen ? "hidden" : "block pt-[56px]"}`}>
+        <div className={`${quoteVarients["base"]} ${quoteVarients[menuState]}`}>
           <p>
             “The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and
             value.”
@@ -19,7 +56,7 @@ export default function Home() {
           <p className={"pt-[13px] font-bold"}>Ada Lovelace</p>
         </div>
 
-        <div className={`flex items-end ${isMenuOpen ? "pb-[56px] pt-[56px]" : "pb-[98px] pt-[233px]"}`}>
+        <div className={`${timeVarients["base"]} ${timeVarients[menuState]}`}>
           <div>
             <div className={"flex items-center gap-x-4"}>
               <SunIcon />
@@ -35,17 +72,19 @@ export default function Home() {
             <p className={"pt-4 text-[24px] font-bold leading-[28px] tracking-[4.8px]"}>IN LONDON, UK</p>
           </div>
           <div className={"ml-auto flex h-[56px] w-[146px] items-center justify-end gap-x-[13px] rounded-[28px] bg-white"}>
-            <span className={"text-[16px] font-bold uppercase leading-[28px] tracking-[5px] text-[#808080]"}>{isMenuOpen ? "Less" : "More"}</span>
+            <span className={"text-[16px] font-bold uppercase leading-[28px] tracking-[5px] text-[#808080]"}>
+              {menuState === "initial" || menuState === "menuClosed" ? "More" : "Less"}
+            </span>
             <button
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              onClick={() => toggleMenu()}
               className={"hover:bg-light-gray mr-[9px] flex size-[40px] items-center justify-center rounded-full bg-dark-gray"}
             >
-              {isMenuOpen ? <ArrowDownIcon /> : <ArrowUpIcon />}
+              {menuState === "initial" || menuState === "menuClosed" ? <ArrowDownIcon /> : <ArrowUpIcon />}
             </button>
           </div>
         </div>
       </div>
-      <div className={"bg-white/50 backdrop-blur-xl"}>
+      <div className={`${subMenuVarients["base"]} ${subMenuVarients[menuState]}`}>
         <div className={"flex px-[165px] py-[74px]"}>
           <div>
             <div>
