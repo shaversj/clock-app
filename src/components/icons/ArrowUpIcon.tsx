a666ba2368242fece0033abe86ac708582164651
Josh Shavers <shaversj@gmail.com>
2024-11-14 07:58:00 -0600

    Update arrows

--- src/components/icons/ArrowUpIcon.tsx
+++ src/components/icons/ArrowUpIcon.tsx
@@ -1,6 +1,6 @@
 export default function ArrowUpIcon() {
   return (
-    <svg className={"h-[8px] w-[12px] md:h-[14px] md:h-[9px]"} viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg className={"h-[8px] w-[12px] md:h-[9px] md:w-[14px]"} viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M1 8L7 2L13 8" stroke="white" strokeWidth="2" />
     </svg>
   );
