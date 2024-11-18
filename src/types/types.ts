28787aea3f74e044bb656dd7f65c751ef8ae8d51
Josh Shavers <shaversj@gmail.com>
2024-11-18 08:48:48 -0600

    Remove unused type

--- src/types/types.ts
+++ src/types/types.ts
@@ -9,24 +9,6 @@ export type IpifyResponse = {
 
 export type MenuState = "initial" | "menuOpen" | "menuClosed";
 
-export type WorldTimeAPIResponse = {
-  utc_offset: string;
-  timezone: string;
-  day_of_week: number;
-  day_of_year: number;
-  datetime: string;
-  utc_datetime: string;
-  unixtime: number;
-  raw_offset: number;
-  week_number: number;
-  dst: boolean;
-  abbreviation: string;
-  dst_offset: number;
-  dst_from: string | null;
-  dst_until: string | null;
-  client_ip: string;
-};
-
 export type TimeAPIResponse = {
   year: number;
   month: number;
